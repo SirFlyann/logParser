5e9edc60e5a13a015058452907d195287c7b216c
Mahezer Carvalho <mahezercarvalho@gmail.com>
2017-06-10 18:27:04 -0300

    Add function to get the players from the game

---
+++ parser/getPlayers.js
@@ -0,0 +1,29 @@
+module.exports = function (el) {
+  // lib to remove duplicate items from arrays
+  var unique = require('array-unique');
+
+  var getKills = require('./getKills.js');
+
+
+  // First we search for lines that have this pattern: n\<Player name>\t\<random-number>
+  var getPlayers = el.match(/n\\.+\\t\\[0-9]/g).map(function(line) {
+    //For each line, remove the "n\" and the "\t\<random-number>"
+    return line.replace("n\\", '').replace(/\\t\\[0-9]/, '');
+  });
+
+  // Not all the players change their info. By the same token, not all players have kills.
+  // So we fuse both filters and voila
+  var kills = getKills(el);
+  var activePlayers = [];
+  kills.forEach(function(kill) {
+    if (kill[0] !== '<world>') {
+      activePlayers.push(kill[0]);
+    }
+    if (kill[1] !== '<world>') {
+      activePlayers.push(kill[1]);
+    }
+  })
+
+  var allPlayers = getPlayers.concat(activePlayers);
+  return unique(allPlayers);
+}
