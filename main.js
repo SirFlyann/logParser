1da8637c103912010f920dc3fa51ced509981926
Mahezer Carvalho <mahezercarvalho@gmail.com>
2017-06-11 17:32:18 -0300

    Add Hapi for the API

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
+'use strict';
+// API lib
+var Hapi = require('hapi');
+
 // lib to manipulate paths with node
 var path = require('path');
 
 // function that manipulates the log file
 var read = require('./parser/read.js');
 
-// Getting the path to the log
-filePath = path.join(__dirname, 'games.log');
+var server = new Hapi.Server();
+server.connection({
+  port: 3000, 
+  host: 'localhost'
+});
 
-// Opening the file and manipulating it;
-var response = read(filePath);
-console.log(response);
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    // Getting the path to the log
+    var filePath = path.join(__dirname, 'games.log');
+
+    // Opening the file and manipulating it;
+    var response = read(filePath);
+    reply(response);
+  }
+})
+
+server.start(function(error) {
+  if (error) {
+    throw error;
+  }
+  
+  console.log('Server running at: ' + server.info.uri);
+});
