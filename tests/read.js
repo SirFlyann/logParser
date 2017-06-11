8eb42ea34b3ee3b7b57d2d024483113cf53c7683
Mahezer Carvalho <mahezercarvalho@gmail.com>
2017-06-11 16:21:34 -0300

    Sort of "namespacing" tests to make reading tests easier

--- tests/read.js
+++ tests/read.js
@@ -3,21 +3,21 @@ var chai = require('chai');
 var expect = chai.expect;
 var read = require('../parser/read.js');
 
-describe('noFile', function() {
+describe('read_NoFile', function() {
   it('should return an error if a file is not passed', function() {
     var readFile = read();
     expect(readFile).to.equal('Path must exist!');
   });
 });
 
-describe('isNotLog', function() {
+describe('read_IsNotLog', function() {
   it('should return an error if the file passed is not a log', function() {
     var readFile = read(path.join(__dirname, 'files/fakeLog.txt'));
     expect(readFile).to.equal('File must be a log file!');
   });
 });
 
-describe('isNotQuake', function() {
+describe('read_IsNotQuake', function() {
   it('should return an error if the file passed is not a quake log', function() {
     var readFile = read(path.join(__dirname, 'files/fakeLog.log'));
     expect(readFile).to.equal('Log must be a Quake file!');
